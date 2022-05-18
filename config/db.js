52d9a28bb2ae967a316ee84c1dcb715e59961b2d
SADoug <douglastec2@gmail.com>
2022-05-18 11:59:21 -0300

    chore: adjust code for deploy on heroku

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+import chalk from "chalk";
+dotenv.config();
+
+let db = null;
+// const mongoClient = new MongoClient("mongodb+srv://teste:teste@clusterdoug.oqldb.mongodb.net/ClusterDoug?retryWrites=true&w=majority");
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+
+try{
+    await mongoClient.connect()
+    db = mongoClient.db(process.env.DATABASE);
+    console.log(chalk.bold.green("Connected to database!"));
+}catch(error){
+    console.log(chalk.bold.red("Could't connet to database!"), error)
+}
+
+export default db;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+
+
+app.listen(process.env.PORT, () => 
+    console.log(chalk.bold.green(`Server online on port ${process.env.PORT}!`))
+);
